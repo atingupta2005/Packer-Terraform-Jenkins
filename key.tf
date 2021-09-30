cf524f144bd44d404ea4f976b1cec397cd447fe4
atingupta2005 <atingupta2005@gmail.com>
2021-09-30 17:42:59 +0530

    Update key.tf

--- key.tf
+++ key.tf
@@ -1,5 +1,5 @@
 resource "aws_key_pair" "mykeypair" {
-    key_name   = "atingupta2005-terraform"
+    key_name   = "atingupta2005-terraform-packer"
     public_key = file(var.PATH_TO_PUBLIC_KEY)
 }
 
